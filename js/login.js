ad8fa3f3cdf2edf2208fd82dd2a5ecc709938c49
DILLIR <uyra908@gmail.com>
2022-11-04 22:52:24 +0100

    changes

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.forms['log_form'].addEventListener('submit', (event) => {
+    event.preventDefault();
+    // TODO do something here to show user that form is being submitted
+    fetch(event.target.action, {
+        method: 'POST',
+        body: new URLSearchParams(new FormData(event.target)) // event.target is the form
+    }).then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // or response.text() or whatever the server sends
+    }).then((body) => {
+        console.log(body);
+    }).catch((error) => {
+        console.log(error);
+    });
+});
